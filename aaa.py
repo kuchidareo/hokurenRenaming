0f7ecadcdeb1297e1c3783abec137a752b4e0b77
kuchidareo <>
2020-06-01 23:32:06 +0900

    あああ

--- aaa.py
+++ aaa.py
@@ -14,17 +14,59 @@ from kivy.clock import Clock
 from watchdog.events import FileSystemEventHandler
 from watchdog.observers import Observer
 
+from PIL import Image
+
 import os,sys
 import threading
 import csv
 import time
+import datetime
 import numpy
 
 resource_add_path("IPAexfont00301")
 LabelBase.register(DEFAULT_FONT, "ipaexg.ttf")
 
+date_today_str = datetime.date.today().strftime('%Y%m%d')
+
 # 監視対象ディレクトリを指定する
 target_dir = 'C:\\Users\\kuchida\\Pictures\\Wireless Transmitter Utility\\D810 2019212\\DCIM\\197ND810'
+
+hokuren_dir = 'C:\\Users\\kuchida\\Desktop\\hokuren'
+h_today_dir = hokuren_dir + '\\' + date_today_str
+resize_image_dir = h_today_dir + '\\resize'
+
+if(not os.path.exists(hokuren_dir)):
+    os.mkdir(hokuren_dir)
+if(not os.path.exists(h_today_dir)):
+    os.mkdir(h_today_dir)
+if(not os.path.exists(resize_image_dir)):
+    os.mkdir(resize_image_dir)
+
+chikudai_dir = 'C:\\Users\\kuchida\\Desktop\\chikudai'
+c_today_dir = chikudai_dir + '\\' + date_today_str
+
+if(not os.path.exists(chikudai_dir)):
+    os.mkdir(chikudai_dir)
+if(not os.path.exists(c_today_dir)):
+    os.mkdir(c_today_dir)
+
+global display_image_path_list
+global hokuren_image_path_list
+global original_image_path_list
+
+display_image_path_list = []
+hokuren_image_path_list = []
+original_image_path_list = []
+
+
+global barcode_list
+global body_number_list
+global carcass_order_list
+
+barcode_list = []
+body_number_list = []
+carcass_order_list = []
+
 
 # FileSystemEventHandler の継承クラスを作成
 class FileChangeHandler(FileSystemEventHandler):
@@ -32,9 +74,9 @@ class FileChangeHandler(FileSystemEventHandler):
      def on_created(self, event):
          filepath = event.src_path
          filename = os.path.basename(filepath)
-         image_src_dir = target_dir + "\\" + filename
-         print(image_src_dir)
-         set_image_src(image_src_dir)
+         if 'JPG' in filename:
+            image_src_dir = target_dir + "\\" + filename
+            resize_and_set_image(image_src_dir)
 
 class LoadDialog(FloatLayout):
     load = ObjectProperty(None)
@@ -59,7 +101,7 @@ class TextWidget(Widget):
         self.carcass_order = '出場順:'
         self.barcode = ''
         self.filepath = ''
-        self.image_src = 'C:\\Users\\kuchida\\Pictures\\_DSC4656 (2).JPG'
+        self.image_src = ''
         Clock.schedule_interval(self.update, 1)
         pass
 
@@ -70,37 +112,84 @@ class TextWidget(Widget):
     def set_image_src(self,src):
         self.image_src = src
 
+    def set_textfocus(self):
+        self.ids.barcode_input.text = ''
+        self.ids.barcode_input_sub.text = ''
+        self.ids.barcode_input.focus = False
+        self.ids.barcode_input_sub.focus = False
+        self.ids.barcode_input.focus = True
+
     def okClicked(self):        # ボタンをクリック時
-        print("hello")
+        print(barcode_list)
+        print(original_image_path_list)
+        print(carcass_order_list)
+        if os.path.exists(c_today_dir+'\\帯広'+date_today_str+'_'+self.body_number[5:]+'.jpg'):
+            os.remove(c_today_dir+'\\帯広'+date_today_str+'_'+self.body_number[5:]+'.jpg')
+        os.rename(original_image_path_list[0],c_today_dir+'\\帯広'+date_today_str+'_'+self.body_number[5:]+'.jpg')
+        if os.path.exists(h_today_dir+'\\'+self.carcass_order[4:]+'_'+self.body_number[5:]+'.jpg'):
+            os.remove(h_today_dir+'\\'+self.carcass_order[4:]+'_'+self.body_number[5:]+'.jpg')
+        os.rename(hokuren_image_path_list[0],h_today_dir+'\\'+self.carcass_order[4:]+'_'+self.body_number[5:]+'.jpg')
+        del display_image_path_list[0:1]
+        del hokuren_image_path_list[0:1]
+        del original_image_path_list[0:1]
+        del barcode_list[0:1]
+        del body_number_list[0:1]
+        del carcass_order_list[0:1]
+
+        if len(display_image_path_list) > 0:
+            self.image_src = display_image_path_list[0]
+        else:
+            self.image_src = ''
+        if len(barcode_list)>0 and len(body_number_list)>0 and len(carcass_order_list)>0:
+            self.barcode = barcode_list[0]
+            self.body_number = '枝肉番号:' + body_number_list[0]
+            self.carcass_order = '出場順:' + carcass_order_list[0]
+        else:
+            self.body_number = '枝肉番号:'
+            self.carcass_order = '出場順:'
+            self.barcode = ''
+
+        self.set_textfocus()
+        
 
     def cancelClicked(self):        # ボタンをクリック時
-        print("cancel")
+        display_image_path_list = []
+        original_image_path_list = []
+        barcode_list = []
+        body_number_list = []
+        carcass_order_list = []
+        self.body_number = '枝肉番号:'
+        self.carcass_order = '出場順:'
+        self.barcode = ''
+        self.image_src = ''
+
+        self.set_textfocus()
 
     def barcodeDetected(self,read_barcode):
-        print(read_barcode)
-        print("barcode detected")
-        if(len(read_barcode) == 13):
-            print(read_barcode[0:4])
-            body_number = read_barcode[0:4]
-            self.barcode = read_barcode
-            self.body_number = '枝肉番号:' + body_number
-            self.ids.barcode_input_sub.focus = True
-            self.ids.barcode_input_sub.text = ''
-            #出場順検索
-            print(carcass_list)
-            self.carcass_order = '出場順:' + str(carcass_list[[d[1] for d in carcass_list].index(body_number)][0])
-        
-    
+        if(len(read_barcode) == 48):
+            body_number = read_barcode[38:42]
+            barcode_list.append(read_barcode)
+            body_number_list.append(body_number)
+            carcass_order_list.append(str(carcass_list[[d[1] for d in carcass_list].index(body_number)][0]))
+            if(self.barcode == ''):
+                self.barcode = read_barcode
+                self.body_number = '枝肉番号:' + body_number
+                self.carcass_order = '出場順:' + str(carcass_list[[d[1] for d in carcass_list].index(body_number)][0])
+        self.ids.barcode_input_sub.text = ''
+        self.ids.barcode_input_sub.focus = True
+
     def barcodeDetectedSub(self,read_barcode):
-        print(read_barcode)
-        print("barcode detected")
-        if(len(read_barcode) == 13):
-            print(read_barcode[0:4])
-            body_number = read_barcode[0:4]
-            self.barcode = read_barcode
-            self.body_number = '枝肉番号:' + body_number  
-            self.ids.barcode_input.focus = True
-            self.ids.barcode_input.text = ''
+        if(len(read_barcode) == 48):
+            body_number = read_barcode[38:42]
+            barcode_list.append(read_barcode)
+            body_number_list.append(body_number)
+            carcass_order_list.append(str(carcass_list[[d[1] for d in carcass_list].index(body_number)][0]))
+            if(self.barcode == ''):
+                self.barcode = read_barcode
+                self.body_number = '枝肉番号:' + body_number
+                self.carcass_order = '出場順:' + str(carcass_list[[d[1] for d in carcass_list].index(body_number)][0])
+        self.ids.barcode_input.text = ''
+        self.ids.barcode_input.focus = True
 
     
     def sansyoClicked(self):
@@ -117,7 +206,6 @@ class TextWidget(Widget):
         load_carcass_list = numpy.loadtxt(fname=load_filepath,skiprows=0,delimiter=",",dtype="U20")[:,0:2]
         for line in load_carcass_list:
             carcass_list.append([line[0],line[1]])
-        print(carcass_list)
         self.ids.barcode_input.focus = True
 
         
@@ -133,6 +221,9 @@ class TestApp(App):
 
     def set_image_src(self,src):
         self.widget.set_image_src(src)
+
+    def image_check(self):
+        return self.widget.image_src
 
 
 def filechangelitener():
@@ -150,17 +241,38 @@ def filechangelitener():
     observer.join()
 
 def set_image_src(src):
-    print("1")
     TestApp().set_image_src(src)
+
+def resize_and_set_image(src):
+    img = Image.open(src)
+    filename = src[src.rfind('\\')+1:]
+    filename = filename[:filename.rfind('.')+1]
+    img = Image.open(src)
+    ##for hokuren
+    img = img.resize((int(img.width / 4), int(img.height / 4)))
+    if img.width > img.height:
+        ##img = img.rotate(-90)
+        img = img.transpose(Image.ROTATE_270)
+
+    img.save(h_today_dir +'\\'+ filename + '.png')
+    ##for display
+    img.save(resize_image_dir +'\\resized_'+ filename + '.jpg')
+
+    original_image_path_list.append(src)
+    hokuren_image_path_list.append(h_today_dir +'\\'+ filename + '.png')
+    display_image_path_list.append(resize_image_dir +'\\resized_'+ filename + '.jpg')
+    if  TestApp().image_check() == '':       
+        set_image_src(resize_image_dir +'\\resized_'+ filename + '.jpg')
+
+    print(original_image_path_list)
+    print(display_image_path_list)
+
 
 
 def app_run(app, a):
     app.run()
 
 if __name__ == '__main__':
-
-
-    
     file_thread = threading.Thread(target=filechangelitener,daemon=True)
     file_thread.start()
 
