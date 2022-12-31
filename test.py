57fa8aff9bdf8aaaa6c6d471ce8452f12dc9016c
reoreoooooo <kuchida@nlp.ist.i.kyoto-u.ac.jp>
2022-12-31 15:13:01 +0900

    Remove unused setting file and codes

--- test.py
+++
@@ -1,8 +0,0 @@
-from PIL import Image, ImageOps
-
-src = 'C:\\Users\\kuchida\\Desktop\\hokuren\\20200622\\ミラー型撮影装置\\_DSC1444.JPG'
-img = Image.open(src)
-img = img.resize((int(img.width / 4), int(img.height / 4)))
-img = img.transpose(Image.ROTATE_180)
-img = ImageOps.mirror(img)
-img.save(src + '_2.jpg')
